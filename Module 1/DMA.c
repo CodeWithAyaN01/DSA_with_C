aaab3b3d5cec996a5b75bffb722efb5b0a2e5359
CodeWithAyaN01 <ayankrrish@gmail.com>
2024-10-02 14:31:39 +0530

    sync

--- Module 1/DMA.c
+++ Module 1/DMA.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-// int main()
-// {
-//     int *ptr;
-//     int a,b;
+int main()
+{
+    int *ptr;
+    int a,b;
 
-//     ptr = (int*)malloc(4*sizeof(int));
-
-//     for (int i = 0 ; i<4 ; i++)
-//     {
-//         ptr[i] = (i*10);
-//     }
-//     for (int i = 0 ; i<4 ; i++)
-//     {
-//         printf("%d\n",ptr[i]);
-//     }
-//     free(ptr);
-// }
+    ptr = (int*)malloc(4*sizeof(int)); // both are same !
+    // ptr = (int*)calloc(4,sizeof(int));
+    for (int i = 0 ; i<4 ; i++)
+    {
+        ptr[i] = (i*10);
+    }
+    for (int i = 0 ; i<4 ; i++)
+    {
+        printf("%d\n",ptr[i]);
+    }
+    free(ptr);
+}
