3b0c519e9b906f1591eaea8744ee898a4731de5c
CodeWithAyaN01 <ayankrrish@gmail.com>
2024-10-03 01:17:56 +0530

    done

--- Module 1/Structure.c
+++ Module 1/Structure.c
@@ -1,12 +1,9 @@
 // #include<stdio.h>
 // int main()
 // {
-//     typedef struct
-//     {
-//         char name[10];
-//         int age;
-//     } humanbeing;
+//     
 
 //     humanbeing p1,p2; // user defined data types;
 //     return 0;
 // }
+
