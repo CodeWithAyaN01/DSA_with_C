c7f919fe99b9b422b5d1f3c4d17fe490ed96a26f
CodeWithAyaN01 <ayankrrish@gmail.com>
2024-10-02 14:15:51 +0530

    symc

---
+++ Module 1/DMA.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+// int main()
+// {
+//     int *ptr;
+//     int a,b;
+
+//     ptr = (int*)malloc(4*sizeof(int));
+
+//     for (int i = 0 ; i<4 ; i++)
+//     {
+//         ptr[i] = (i*10);
+//     }
+//     for (int i = 0 ; i<4 ; i++)
+//     {
+//         printf("%d\n",ptr[i]);
+//     }
+//     free(ptr);
+// }
--- Module 1/pointer.c
+++ Module 1/pointer.c
@@ -1,11 +1,28 @@
-#include<stdio.h>
-// basics
-int main()
-{
-    int *p;
-    int a = 6;
-    p = &a;
-    printf("%d\n",*p);
-    printf("%d",a);
-    return 0;
-}
+// #include<stdio.h>
+// // basics
+// int main()
+// {
+//     int *p;
+//     int a = 6;
+//     p = &a;
+//     printf("%d\n",*p);
+//     printf("%d",a);
+//     return 0;
+// }
+
+//ARRAY TROUGH POINTER;
+
+// #include<stdio.h>
+// int main()
+// {
+//     int a[5] = {1, 2, 3, 4, 5};
+//     int *p = a; // same as int*p = &a[0]
+//     for (int i=0; i<5; i++)
+//     {
+//         printf("%d\t", *p); // printing the value in the address:
+//         p++; // updating the address 
+//     }
+//     return 0;
+// }
+
+
