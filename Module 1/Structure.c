b6992848cc2a36f0dbd590206438505b66c01158
CodeWithAyaN01 <ayankrrish@gmail.com>
2024-10-03 11:37:33 +0530

    Structures

--- Module 1/Structure.c
+++ Module 1/Structure.c
@@ -1,9 +1,29 @@
-// #include<stdio.h>
-// int main()
-// {
-//     
+#include<stdio.h>
+#include<stdlib.h>
 
-//     humanbeing p1,p2; // user defined data types;
-//     return 0;
-// }
+void EnterDetails(char CarVerson[20] , int CarAge , int CarPrice);
+int main()
+{
+    typedef struct 
+    {
+        char CarVerson[50];
+        int CarAge;
+        int CarPrice;
+    }car;
+    
+    car skoda,aulto; // user defined function;
 
+    EnterDetails(skoda.CarVerson , skoda.CarAge , skoda.CarPrice);
+    printf("\n\n");
+    EnterDetails(aulto.CarVerson , aulto.CarAge , aulto.CarPrice);
+
+}
+void EnterDetails(char CarVerson[20] , int CarAge , int CarPrice)
+{
+    printf("enter the car name: ");scanf("%s",CarVerson);
+    printf("\nEnter the car Age : ");scanf("%d",&CarAge);
+    printf("\nEnter the car price: ");scanf("%d",&CarPrice);
+    system("cls");
+    printf("Car is %s its age is %d and its price is %d",CarVerson,CarAge,CarPrice);
+    
+}
