ae6a50b14ace7cfda3c2671f22f6af197e03c1e7
CodeWithAyaN01 <ayankrrish@gmail.com>
2024-10-08 01:19:27 +0530

    text;

--- Module 1/c.c
+++ Module 1/c.c
@@ -1,22 +1,42 @@
-#include<stdio.h>
-#include<stdlib.h>
-int main()
-{
-    int *ptr;
-    int arr[10];
-    int size = 6;
-    ptr = &arr; // assigning values to ptr
-    ptr = (int*)malloc(size * sizeof(int));
-    printf("Enter arr values: ");
-    for (int i = 0 ; i<size ; i++)
-    {
-        scanf("%d",&ptr[i]);
-    }
-    printf("printing values \n");
-    for (int i = 0 ; i<size ; i++)
-    {
-        printf("%d \t",*ptr);
-        ptr++;
-    }
-    return 0;
-}
+// #include<stdio.h>
+// #include<stdlib.h>
+// int main()
+// {
+//     int *ptr;
+//     int arr[10];
+//     int size = 6;
+//     ptr = &arr; // assigning values to ptr
+//     ptr = (int*)malloc(size * sizeof(int));
+//     printf("Enter arr values: ");
+//     for (int i = 0 ; i<size ; i++)
+//     {
+//         scanf("%d",&ptr[i]);
+//     }
+//     printf("printing values \n");
+//     for (int i = 0 ; i<size ; i++)
+//     {
+//         printf("%d \t",*ptr);
+//         ptr++;
+//     }
+//     return 0;
+// }
+
+// #include<stdio.h>
+// #include<stdlib.h>
+// int main()
+// {
+//     char *text;
+//     char name[20];
+//     int length;
+//     printf("enter length of text: ");scanf("%d",&length);
+//     text = (char*)calloc(length+1,sizeof(char));
+//     text = &name;
+//     printf("Enter name: ");
+//     gets(name);
+//     for (int i = 0 ; i<length ; i++)
+//     {
+//         printf("%c",*text);
+//         text++;
+//     }    
+//     return 0;
+// }
