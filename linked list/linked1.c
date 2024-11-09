ae59a0e8f033d86ccacb725bee12e8f8a1310c80
CodeWithAyaN01 <ayankrrish@gmail.com>
2024-11-09 18:28:34 +0530

    commmit

--- linked list/linked1.c
+++ linked list/linked1.c
@@ -12,7 +12,8 @@
 //     head->data = 55;
 //     head->link = NULL;
 // }
-
+#include<stdlib.h>
+#include<stdio.h>
 struct node 
 {
     int data;
@@ -20,5 +21,19 @@ struct node
 };
 int main()
 {
-    struct node *head = malloc(sizeof(struct node));
+    struct node *Nodes[5];
+    // memory Allocate in heap 
+    //  this step creates 3 Nodes 
+    struct node *head = (struct node*)malloc(sizeof(struct node));
+    struct node *second= (struct node*)malloc(sizeof(struct node));
+    struct node *third= (struct node*)malloc(sizeof(struct node));
+
+    head->data = 20;
+    head->link = second;
+    second->data = 30;
+    second->link = third;
+    third->data = 40;
+    third->link = NULL;
+    printf("Program is running fine: ");
 }
+
---
+++ linked list/linked3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int data;
+    struct node *next;
+};
+int main()
+{
+    struct node *list[5];
+    for (int i = 0 ; i<5 ; i++)
+    {
+        list[i] = malloc(sizeof(struct node)); //  created multiple node 
+    }
+
+    for (int i = 0 ; i<5 ; i++)
+    {
+        list[i]->data = i+1;
+        list[i]->next = list[i+1]; 
+    }
+    for (int i = 0 ; i<5 ; i++)
+    {
+        printf("The data of node[%d] is = %d\n",i,list[i]->data);
+    }
+    printf("\nProgram runs fine: ");
+}
