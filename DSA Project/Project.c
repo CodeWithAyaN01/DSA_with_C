f7e0fa284827791a89fc3ee62fc1ce340109ee63
CodeWithAyaN01 <ayankrrish@gmail.com>
2024-11-18 18:26:07 +0530

    till now

--- DSA Project/Project.c
+++ DSA Project/Project.c
@@ -1,15 +1,43 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-void Add_Patient()
-{
-    printf("Enter Name of the Patient: ");
-}
+void Add_Patient();
+void Discharge_Patient();
+void Display_List();
+void Time(char* FormattedDate);
 struct Patient
 {
-    char Name[30];
+    char P_Name[30];
     int Patient_ID;
     char Patient_Address[30];
     char Patient_Problem[30];
     char Patient_Date[20];
-}p[10];
+}p;
+int main()
+{
+
+}
+void Add_Patient()
+{
+    char Date_of_Admit[20];
+    printf("\nEnter Name of the Patient: ");gets(p.P_Name);
+    printf("\n Enter the Patinet ID: ");gets(p.Patient_ID);
+    printf("\n Enter the Patient Address: ");gets(p.Patient_Address);
+    Time(p.Patient_Date);
+
+}
+void Time(char* FormattedDate)
+{
+    time_t currentTime;
+    struct tm *local_Time; 
+    time(&currentTime);
+    //local time representation
+    local_Time = localtime(&currentTime);
+    sprintf(FormattedDate,"%02d/%02d/%04d", local_Time->tm_mday , local_Time->tm_mon+1,local_Time->tm_year+1900);;
+}
+void Display_List()
+{
+    
+}
+
+
