8f61793556a904be7113e5e4e7023bb10c86a2b7
CodeWithAyaN01 <ayankrrish@gmail.com>
2024-11-18 15:29:44 +0530

    Solve:

--- DSA Project/Project.c
+++ DSA Project/Project.c
@@ -1,3 +1,15 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+void Add_Patient()
+{
+    printf("Enter Name of the Patient: ");
+}
+struct Patient
+{
+    char Name[30];
+    int Patient_ID;
+    char Patient_Address[30];
+    char Patient_Problem[30];
+    char Patient_Date[20];
+}p[10];
