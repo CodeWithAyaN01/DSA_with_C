089a2767a10c1ee2593012c97a8b2b0444fa7fd7
CodeWithAyaN01 <ayankrrish@gmail.com>
2025-04-26 14:15:57 +0530

    Commited

---
+++ Algo/Insertion_sort.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+void insertionSort(int A[] , int n) {
+    for (int i = 1 ; i<n ; i++) {
+        int current = A[i];
+        int prev = i-1;
+        while (prev>=0 && A[prev] >= current) {
+            A[prev+1] = A[prev]; //  current Exchanged to prev
+            prev--;
+        }
+        A[prev+1] = current;
+    }
+}
+int main() {
+    int A[] = {1,4,6,3,0,5};
+    int n = 6;
+    insertionSort(A,n);
+    for (int i= 0 ; i<n ; i++) {
+        printf("%d ",A[i]);
+    }
+
+
+}
+
